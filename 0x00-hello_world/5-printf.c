6cd731a647790ee61c6523fe9c1f0afcb0213bc2
brianmaleek <maleekmalik@gmail.com>
2023-02-16 18:12:37 -0800

    C program that prints using the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str1);
+	return (0);
+}
