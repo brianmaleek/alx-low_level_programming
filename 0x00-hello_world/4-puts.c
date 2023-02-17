8aec39dce5a93e08d7efe20c7c2880d686274ee6
brianmaleek <maleekmalik@gmail.com>
2023-02-16 17:51:35 -0800

    C program prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char str1[] = "Programming is like building a multilingual puzzle";
+puts(str1);
+return (0);
+}
