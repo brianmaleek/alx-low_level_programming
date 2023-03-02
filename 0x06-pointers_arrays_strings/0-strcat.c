ec53b29701fa0436ee6fdb623e3257e556e87dd6
brianmaleek <maleekmalik@gmail.com>
2023-03-02 23:04:30 +0300

    Test files aded

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: string @src is added to
+ * @src: string to be appended
+ * Returns: a pointer to the resulting string @dest
+*/
+
