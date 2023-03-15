8da97de01a8c4ee74372376cf10032bc7b7d9b9a
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-03-15 05:10:41 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int len1 = 0, len2 = 0, i = 0, j = 0;
+	int len1 = 0, len2 = 0, i = 0, j = 0, len;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,7 +23,9 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j])
 		len2++, j++;
 
-	concat = malloc(sizeof(char) * (len1 + len2));
+	len = len1 + len2;
+
+	concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
 		return (NULL);
@@ -31,8 +33,9 @@ char *str_concat(char *s1, char *s2)
 	for (; i < len1; i++)
 		concat[i] = s1[i];
 
-	for (j = len1; j = len1, i = len2; j++, i++)
-		concat[j] = s2[i];
+	for (; j < len2; j++, i++)
+		concat[i] = s2[j];
+	concat[len] = '\0';
 
 	return (concat);
 }
