e62c050c25c69a1177503cb421616ad48f96e534
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-04-09 09:35:38 +0300

    function converts binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,22 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0')
+		{
+			num <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			num = (num << 1) | 1;
+		}
+		else
 		{
 			return (0);
-		}
-
-		num = num << 1;
-		if (b[i] == '1')
-		{
-			num = num | 1;
 		}
 	}
 	return (num);
