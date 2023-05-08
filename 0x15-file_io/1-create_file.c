18abd0367028e302ee7686724baa0491bfed40eb
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-05-08 23:31:09 +0300

    1-create_file updated checker happiness

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor = -1, bytes_written = 0, len_text_content = 0;
+	int file_descriptor = -1, bytes_written = 0;
 
 	if (filename == NULL)
 	{
@@ -28,19 +28,16 @@ int create_file(const char *filename, char *text_content)
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMS);
 	if (file_descriptor == -1)
 	{
-		dprintf(stderr, "Error: cannot open file %s\n", filename);
+		fprintf(stderr, "Error: cannot open file %s\n", filename);
 		return (-1);
 	}
 
 	if (text_content != NULL)
 	{
-		while (text_content[len_text_content])
-			len_text_content++;
-
-		bytes_written = write(file_descriptor, text_content, len_text_content);
+		ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
-			dprintf(stderr, "Error: cannot write file %s\n", filename);
+			fprintf(stderr, "Error: cannot write file %s\n", filename);
 			close(file_descriptor);
 			return (-1);
 		}
