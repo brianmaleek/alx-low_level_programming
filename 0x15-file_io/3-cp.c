cc7995f9427560482c463bfa574d9b4d7ecc768a
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-05-09 02:41:37 +0300

    3-cp.c update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,13 +4,14 @@
  * exitWithError - prints error message with exit value
  * @error: exit value
  * @message: error message
- * @file_descriptor: file descriptor to close
+ * @arg: additional argument for format specifier
+ * @fd: file descriptor to close
  */
-void exitWithError(int error, const char *message, int file_descriptor)
+void exitWithError(int error, const char *message, const char *arg, int fd)
 {
-	dprintf(STDERR_FILENO, message, file_descriptor);
-	if (file_descriptor != -1)
-		close(file_descriptor);
+	dprintf(STDERR_FILENO, message, arg);
+	if (fd != -1)
+		close(fd);
 	exit(error);
 }
 
@@ -22,35 +23,35 @@ void exitWithError(int error, const char *message, int file_descriptor)
  */
 int main(int argc, char *argv[])
 {
-	int file_descriptor1, file_descriptor2, n_read, n_write;
+	int fd1, fd2, n_read, n_write;
 	char buffer[1024];
 
 	if (argc != 3)
-		exitWithError(97, "Usage: cp file_from file_to\n", -1);
+		exitWithError(97, "Usage: cp file_from file_to\n", NULL, -1);
 
-	file_descriptor1 = open(argv[1], O_RDONLY);
-	if (file_descriptor1 == -1)
-		exitWithError(98, "Error: Can't read from file %s\n", -1);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		exitWithError(98, "Error: Can't read from file %s\n", argv[1], -1);
 
-	file_descriptor2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_descriptor2 == -1)
-		exitWithError(99, "Error: Can't write to %s\n", file_descriptor1);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		exitWithError(99, "Error: Can't write to %s\n", argv[2], fd1);
 
-	while ((n_read = read(file_descriptor1, buffer, sizeof(buffer))) > 0)
+	while ((n_read = read(fd1, buffer, sizeof(buffer))) > 0)
 	{
-		n_write = write(file_descriptor2, buffer, n_read);
+		n_write = write(fd2, buffer, n_read);
 		if (n_write == -1 || n_write != n_read)
-			exitWithError(99, "Error: Can't write to %s\n", file_descriptor2);
+			exitWithError(99, "Error: Can't write to %s\n", argv[2], fd2);
 	}
 
 	if (n_read == -1)
-		exitWithError(98, "Error: Can't read from file %s\n", file_descriptor1);
+		exitWithError(98, "Error: Can't read from file %s\n", argv[1], fd1);
 
-	if (close(file_descriptor1) == -1)
-		exitWithError(100, "Error: Can't close fd %d\n", file_descriptor1);
+	if (close(fd1) == -1)
+		exitWithError(100, "Error: Can't close fd %d\n", argv[1], fd1);
 
-	if (close(file_descriptor2) == -1)
-		exitWithError(100, "Error: Can't close fd %d\n", file_descriptor2);
+	if (close(fd2) == -1)
+		exitWithError(100, "Error: Can't close fd %d\n", argv[2], fd2);
 
 	return (0);
 }
