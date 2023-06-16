ef22c94f807046bc7094d27ac3b296feeb52b778
brianmaleek <maleekmalik@gmail.com>
2023-06-17 01:33:43 +0300

    Que 0 solved

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to head of list
- * Return: number of nodes
+  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -22,4 +22,3 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 	return (count);
 }
-
