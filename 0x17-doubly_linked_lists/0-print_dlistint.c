9a7781d04c641e9a39ec88c59dee7688a0f3a351
brianmaleek <maleekmalik@gmail.com>
2023-06-17 03:30:49 +0300

    Que 0.00 solved

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,16 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (count);
-	}
-
 	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count += 1;
 	}
 	return (count);
 }
