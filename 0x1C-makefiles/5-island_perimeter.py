01a9c703892f8612cfcd41e2a930678a217bf1e3
brianmaleek <maleekmalik@gmail.com>
2023-08-22 01:07:50 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module Island Perimeter
+"""
 
 
 def island_perimeter(grid):
@@ -22,12 +25,16 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
+                # count top edge
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
+                #count left edge
                 if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
+                # count bottom edge
                 if row == rows - 1 or grid[row + 1][col] == 0:
                     perimeter += 1
+                # count right edge
                 if col == cols - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
